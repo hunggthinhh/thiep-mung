391abb07ba5822d8ac8bdbbb1da90d0eea5d2cf4
hunggthinhh <151088407+hunggthinhh@users.noreply.github.com>
2024-11-17 19:02:44 +0700

    Add files via upload
    create card

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Bạn';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100014633947426';
+const eventName = 'Chúc Mừng Sinh Nhật';
+const titleCard = 'Tặng Cậu';
+const contentCard = 'Chúc Nguyễn Thành Đạt sinh nhật zui zẻ. Tuổi mới thêm nhiều niềm vui mới cầu gì được nấy Đạt nhé. Cầu tiền được tiền cầu tài được tài cầu tình đc tình háp pi pớt đây mãi yêu ông chúc Đạt sống mãi trong sánh ánh của 10 chương dương phật Đạt nhé';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
